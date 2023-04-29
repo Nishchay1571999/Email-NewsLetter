36703aa909b9c35856f05d9b0c106bffe2277000
Nishchay Bhatt <bhatt.nishchay@royalbrothers.com>
2023-04-29 16:36:16 +0530

    My first Rust server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get,HttpResponse,HttpServer,Responder,App};
+
+
+#[get("/")]
+async fn hello() -> impl Responder{
+    HttpResponse::Ok().body("Hello World!")
 }
+
+#[get("/echo")]
+async fn manual_hello() -> impl Responder{
+    HttpResponse::Ok().body("Hello World!!!!")
+}
+
+
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()>{
+    HttpServer::new(||
+    {
+        App::new()
+            .service(hello)
+            .service(manual_hello)
+    })
+    .bind(("127.0.0.1",8080))?
+    .run()
+    .await
+}
+
+
